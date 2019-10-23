8020442dd168b0d65620d54a6a8ca867a4d8ba96
倪如霖 <frank61003@yahoo.com.tw>
2019-10-23 11:47:47 +0800

    def

--- HarshStoneCloud/src/gameObject/CardFactory.java
+++ HarshStoneCloud/src/gameObject/CardFactory.java
@@ -67,10 +67,11 @@ public class CardFactory {
         
         
         }
-        System.out.print("456");
+        
         System.out.print("123");
         System.out.print("789");       
-        System.out.print("abc");       
+        System.out.print("abc");      
+        System.out.print("def");  
         return carddata;
     }
     
